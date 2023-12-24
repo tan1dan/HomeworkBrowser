d8be4f8e2c833c76b98020840c83255e8e38a64d
Ivan Znak <ivanznak04@gmail.com>
2023-12-24 19:25:37 +0100

    Interface was added to ViewController

--- HomeworkBrowser/HomeworkBrowser/ViewController.swift
+++ HomeworkBrowser/HomeworkBrowser/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, TargetViewDelegate {
+    let webView = WKWebView()
+    let targetView = TargetView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(webView)
+        view.addSubview(targetView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        targetView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            targetView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            targetView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10),
+            targetView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -10),
+            targetView.bottomAnchor.constraint(equalTo: webView.topAnchor, constant: 5),
+            
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
-
-
+    
+    func buttonBackPressed(_ sender: TargetView) {
+        <#code#>
+    }
+    
+    func buttonForwardPressed(_ sender: TargetView) {
+        <#code#>
+    }
+    
+    func buttonRestartPressed(_ sender: TargetView) {
+        <#code#>
+    }
+    
+    func buttonSearchPressed(_ sender: TargetView) {
+        <#code#>
+    }
+    
 }
 
