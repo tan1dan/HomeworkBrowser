aa83145de2119dee8ee0bd301f03083e83d0ef7b
Ivan Znak <ivanznak04@gmail.com>
2023-12-24 18:16:36 +0100

    Project was added

---
+++ HomeworkBrowser/HomeworkBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkBrowser
+//
+//  Created by Иван Знак on 24/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
